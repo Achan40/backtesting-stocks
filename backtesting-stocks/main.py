dceedef612be5c4833591749aa21e8b058531236
Aaron <chanman2841@gmail.com>
2021-05-03 22:38:14 -0500

    very basic backtest written and performed

--- backtesting-stocks/main.py
+++ backtesting-stocks/main.py
@@ -6,12 +6,22 @@ import pandas as pd
 import requests
 import json
 
+# percentage change function
+def get_change(final, initial):
+        if final == initial:
+            return 100.0
+        try:
+            return (abs(final - initial) / initial) * 100.0
+        except ZeroDivisionError:
+            return 0
+
 # Stock object, requires a symbol to be created
 class Stock:
 
-    def __init__(self, symbol):
+    def __init__(self, symbol, starting_cash):
         self.symbol = symbol
-        self.acc_bal = 10000
+        self.acc_cash_initial = starting_cash
+        self.acc_cash = starting_cash
         self.num_stock = 0
         self.num_buys = 0
         self.num_failed_buys = 0
@@ -36,8 +46,8 @@ class Stock:
 
     # private buy method
     def __buy_stock(self, stockval):
-        if(self.acc_bal >= stockval):
-            self.acc_bal = self.acc_bal - stockval
+        if(self.acc_cash >= stockval):
+            self.acc_cash = self.acc_cash - stockval
             self.num_stock += 1
             self.num_buys += 1
         else:
@@ -46,17 +56,36 @@ class Stock:
     # private sell method
     def __sell_stock(self, stockval):
         if(self.num_stock > 0):
-            self.acc_bal = self.acc_bal + stockval
+            self.acc_cash = self.acc_cash + stockval
             self.num_stock -= 1
             self.num_sells += 1
         else:
             self.num_nothing_to_sell += 1
 
-    # total account value method
-    def get_total_val(self, final_stockval):
-        self.acc_total = self.num_stock * final_stockval + self.acc_bal
-        print(self.acc_total)
-        print(self.num_stock)
+    
+
+    # final values and percentage calculations
+    def get_finals(self):
+        stock_initial_value = self.prices["close"].iloc[0]
+        stock_final_value = self.prices["close"].iloc[-1]
+
+        self.acc_total = self.num_stock * stock_final_value + self.acc_cash
+        self.stock_gain_per = get_change(stock_final_value, stock_initial_value)
+        self.acc_gain_per = get_change(self.acc_total, self.acc_cash_initial)
+
+
+    # print totals
+    def get_totals(self):
+        print("Ending cash: ", self.acc_cash)
+        print("Total number of stock held: ", self.num_stock)
+        print("Total number of purchases: ", self.num_buys)
+        print("Total number of sells: ", self.num_sells)
+        print("Total number of failed buys (out of funds): ", self.num_failed_buys)
+        print("Total number of failed sells (out of stock): ", self.num_nothing_to_sell)
+        print("Total ending account value: ", self.acc_total)
+        print("Stock percentage gain: ", self.stock_gain_per)
+        print("Portfolio percentage gain: ", self.acc_gain_per)
+
 
     def backtest1(self):
         num_rows = self.prices.shape[0]
@@ -65,12 +94,13 @@ class Stock:
                 self.__sell_stock(self.prices["close"][i])
             if(self.prices["changePercent"][i] < -.01):
                 self.__buy_stock(self.prices["close"][i])
-        self.get_total_val(self.prices["close"].iloc[-1])
+        self.get_finals()
+        self.get_totals()
         
         
 
 def main():
-    AAPL = Stock("AAPL")
+    AAPL = Stock("AAPL", 10000)
     AAPL.get_df_prices("1y")
     print(AAPL.prices)
     AAPL.backtest1()
