208705824c53ec3623b18867348d8b589f37ac1e
Aaron <chanman2841@gmail.com>
2021-05-03 21:28:08 -0500

    created first backtest

--- backtesting-stocks/main.py
+++ backtesting-stocks/main.py
@@ -1,6 +1,7 @@
 # import variable that stores my API key located in a .py file 
 from sandbox import SECRET_KEY
 
+import numpy as np
 import pandas as pd
 import requests
 import json
@@ -12,6 +13,10 @@ class Stock:
         self.symbol = symbol
         self.acc_bal = 10000
         self.num_stock = 0
+        self.num_buys = 0
+        self.num_failed_buys = 0
+        self.num_sells = 0
+        self.num_nothing_to_sell = 0
 
     # method returns a dataframe of stock prices for that symbol 
     def get_df_prices(self, date_param='10d'):
@@ -34,33 +39,41 @@ class Stock:
         if(self.acc_bal >= stockval):
             self.acc_bal = self.acc_bal - stockval
             self.num_stock += 1
+            self.num_buys += 1
         else:
-            print("Out of funds")
-            return
+            self.num_failed_buys += 1
 
     # private sell method
     def __sell_stock(self, stockval):
         if(self.num_stock > 0):
             self.acc_bal = self.acc_bal + stockval
             self.num_stock -= 1
+            self.num_sells += 1
         else:
-            print("None to sell")
-            return
+            self.num_nothing_to_sell += 1
 
     # total account value method
     def get_total_val(self, final_stockval):
         self.acc_total = self.num_stock * final_stockval + self.acc_bal
         print(self.acc_total)
+        print(self.num_stock)
 
     def backtest1(self):
-        TEST = 'TEST'
+        num_rows = self.prices.shape[0]
+        for i in range(0,num_rows):
+            if(self.prices["changePercent"][i] > .01):
+                self.__sell_stock(self.prices["close"][i])
+            if(self.prices["changePercent"][i] < -.01):
+                self.__buy_stock(self.prices["close"][i])
+        self.get_total_val(self.prices["close"].iloc[-1])
         
         
 
 def main():
     AAPL = Stock("AAPL")
-    AAPL.get_df_prices("2y")
+    AAPL.get_df_prices("1y")
     print(AAPL.prices)
+    AAPL.backtest1()
 
 if __name__ == "__main__":
     main()
