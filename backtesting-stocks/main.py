90de49bb6e6ebb59778d1766e0c0e6af890f8f8e
Aaron <chanman2841@gmail.com>
2021-05-10 17:23:17 -0500

    algo purchases more when there are larger differences in change

--- backtesting-stocks/main.py
+++ backtesting-stocks/main.py
@@ -16,12 +16,14 @@ def get_change(final, initial):
             return 0
 
 # Stock object, requires a symbol to be created
-class Stock:
-
-    def __init__(self, symbol, starting_cash):
-        self.symbol = symbol
+class Account:
+    # Constructor, requires some amount of starting cash, the timeframe for the backtest, and a list of symbols
+    def __init__(self, starting_cash, timeframe, symbol):
         self.acc_cash_initial = starting_cash
         self.acc_cash = starting_cash
+        self.timeframe = timeframe
+        self.symbol = symbol
+
         self.num_stock = 0
         self.num_buys = 0
         self.num_failed_buys = 0
@@ -29,11 +31,11 @@ class Stock:
         self.num_nothing_to_sell = 0
 
     # method returns a dataframe of stock prices for that symbol 
-    def get_df_prices(self, date_param='10d'):
+    def get_df_prices(self):
         url_prefix = "https://sandbox.iexapis.com/stable/"
 
         # Gets the stock price at closing for some date parameter
-        path = f'stock/{self.symbol}/chart/{date_param}?chartCloseOnly=True&&token={SECRET_KEY}'
+        path = f'stock/{self.symbol}/chart/{self.timeframe}?chartCloseOnly=True&&token={SECRET_KEY}'
         full_url = requests.compat.urljoin(url_prefix, path)
 
         # request 
@@ -44,7 +46,7 @@ class Stock:
         self.prices = pd.DataFrame(prices_obj)
         return self.prices
 
-    # private buy method
+    # buy a single share of stock
     def __buy_stock(self, stockval):
         if(self.acc_cash >= stockval):
             self.acc_cash = self.acc_cash - stockval
@@ -53,7 +55,7 @@ class Stock:
         else:
             self.num_failed_buys += 1
 
-    # private sell method
+    # sell a single share of stock
     def __sell_stock(self, stockval):
         if(self.num_stock > 0):
             self.acc_cash = self.acc_cash + stockval
@@ -84,20 +86,25 @@ class Stock:
         print("Stock percentage gain: ", self.stock_gain_per)
         print("Portfolio percentage gain: ", self.acc_gain_per)
 
-
+    # backtesting algorithm
     def backtest1(self):
         num_rows = self.prices.shape[0]
         for i in range(0,num_rows):
-            if(self.prices["changePercent"][i] > .01):
-                self.__sell_stock(self.prices["close"][i])
-            if(self.prices["changePercent"][i] < -.01):
-                self.__buy_stock(self.prices["close"][i])
+            pdelta = self.prices["changePercent"][i]
+            if(pdelta > .05):
+                nsells = int(pdelta/.05)
+                for j in range(0,nsells):
+                    self.__sell_stock(self.prices["close"][i])
+            if(pdelta < -.01):
+                nbuys = int(pdelta/-.01)
+                for k in range(0, nbuys):
+                    self.__buy_stock(self.prices["close"][i])
         self.get_finals()
         self.get_totals()
 
 def main():
-    AAPL = Stock("AAPL", 10000)
-    AAPL.get_df_prices("1y")
+    AAPL = Account(10000,"5y","AAPL")
+    AAPL.get_df_prices()
     print(AAPL.prices)
     AAPL.backtest1()
 
